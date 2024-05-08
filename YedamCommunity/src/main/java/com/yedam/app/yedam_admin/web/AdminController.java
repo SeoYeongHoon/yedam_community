0ea07e4f408ae051024a22f1c935f3c19506795c
SeoYeongHoon <tjdudgns@naver.com>
2024-05-08 12:35:02 +0900

    공통 페이지 헤더 메뉴, 어드민 페이지 헤더(사이드) 메뉴 업데이트

---
+++ YedamCommunity/src/main/java/com/yedam/app/yedam_admin/web/AdminController.java
@@ -0,0 +1,13 @@
+package com.yedam.app.yedam_admin.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+	@GetMapping("/admin")
+	public String homePage() {
+		return "admin";
+		//classpath:/templates/admin.html
+	}
+}
