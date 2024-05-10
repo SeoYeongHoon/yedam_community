da64f7e04b7db6d36cf4eab33b840b198a205973
SeoYeongHoon <tjdudgns@naver.com>
2024-05-10 17:24:10 +0900

    로그인, 회원가입, 메인페이지 개선

--- YedamCommunity/src/main/java/com/yedam/app/yedam_start/web/StartController.java
+++ YedamCommunity/src/main/java/com/yedam/app/yedam_start/web/StartController.java
@@ -1,13 +1,13 @@
-package com.yedam.app.yedam_start.web;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class StartController {
-	@GetMapping("/")
-	public String startPage() {
-		return "mainPages/start";
-		//classpath:/templates/start.html
-	}
-}
+//package com.yedam.app.yedam_start.web;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class StartController {
+//	@GetMapping("/")
+//	public String startPage() {
+//		return "mainPages/start";
+//		//classpath:/templates/start.html
+//	}
+//}
--- YedamCommunity/src/main/resources/static/js/admin.js
+++ YedamCommunity/src/main/resources/static/js/admin.js
@@ -52,3 +52,15 @@
   });*/
 })();
 
+$(document).ready(function(){
+	    $('.password_input i').on('click',function(){
+	        $('input').toggleClass('active');
+	        if($('input').hasClass('active')){
+	            $(this).attr('class',"fa fa-eye-slash fa-lg")
+	            .prev('input').attr('type',"text");
+	        }else{
+	            $(this).attr('class',"fa fa-eye fa-lg")
+	            .prev('input').attr('type','password');
+	        }
+	    });
+	});
