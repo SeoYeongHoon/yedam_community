a68af9e81735a2ee8464b8c7fa8845f9c8c2c257
syh <syh@192.168.219.100>
2024-05-13 23:48:53 +0900

    아이디 비번찾기 페이지 기초구현, 회원가입 기능 보충

---
+++ YedamCommunity/src/main/resources/static/js/register.js
@@ -0,0 +1,27 @@
+function showImage() {
+	var newImage = document.getElementById('image-show').lastElementChild;
+	newImage.style.visibility = "visible";
+
+	document.getElementById('image-upload').style.visibility = 'hidden';
+
+	/*document.getElementById('fileName').textContent = null; */    //기존 파일 이름 지우기
+}
+
+function loadFile(input) {
+	var file = input.files[0];
+
+	/*var name = document.getElementById('fileName');
+	name.textContent = file.name;*/
+
+	var newImage = document.createElement("img");
+	newImage.setAttribute("class", 'img');
+
+	newImage.src = URL.createObjectURL(file);
+
+	newImage.style.width = "70%";
+	newImage.style.height = "70%";
+	newImage.style.objectFit = "contain";
+
+	var container = document.getElementById('image-show');
+	container.appendChild(newImage);
+};
