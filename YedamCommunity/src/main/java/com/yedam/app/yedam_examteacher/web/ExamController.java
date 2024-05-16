b935eeee2fd2e130b780a76e1a6c8f2571b529a0
LTJ <dlxorwns@naver.com>
2024-05-17 00:57:47 +0900

    0516 LTJ 집에서 작업 - 문제 저장소 문제 저장소 자세히보기 눌렀을때 문제랑 지문 뜨게 수정중.

--- YedamCommunity/src/main/java/com/yedam/app/yedam_examteacher/web/ExamController.java
+++ YedamCommunity/src/main/java/com/yedam/app/yedam_examteacher/web/ExamController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.yedam.app.yedam_examteacher.service.ExamService;
 import com.yedam.app.yedam_examteacher.service.TeacherVO;
 
@@ -21,17 +19,26 @@ public class ExamController {
 	@Autowired
 	ExamService examService;
 
-	// 등록된 시험리스트
+	// 등록된 시험리스트 출력
 	@GetMapping("testlist")
 	public String examList(Model model) {
 		List<TeacherVO> list = examService.testList();
 		model.addAttribute("testList", list);
 		return "cbt_teacher/testlist";
 	}
+	
+	// 시험 등록 - 페이지
+	@GetMapping("testinsert")
+	public String testInsertForm(Model model) {
+		model.addAttribute("teacherVO", new TeacherVO());
+		return "cbt_teacher/insertTest";
+	}
 
 	// 문제 등록 - 페이지
 	@GetMapping("quizinsert")
 	public String quizInsertForm(Model model) {
+		List<TeacherVO> list = examService.subjectList();
+		model.addAttribute("subjectList", list);
 		model.addAttribute("teacherVO", new TeacherVO());
 		return "cbt_teacher/insertquiz";
 	}
@@ -43,25 +50,24 @@ public class ExamController {
 		return "redirect:quizlist";
 	}
 
-	// 문제 저장소에 여러가지 값들 출력
+	// 문제 저장소 페이지
 	@GetMapping("quizlist")
 	public String quizList(TeacherVO teacherVO, Model model) {
-		// List<TeacherVO> list1 = examService.quizList();
-		List<TeacherVO> list2 = examService.answerList(teacherVO);
+		
+		List<TeacherVO> list2 = examService.answerList1(teacherVO);
 		List<TeacherVO> list3 = examService.subjectList();
 
-		//TeacherVO findVO = examService.quizInfo(teacherVO);
-		//model.addAttribute("quizInfo", findVO);
-
-		// Gson gson = new GsonBuilder().create();
-		// String json1 = gson.toJson(list1);
-
-		// model.addAttribute("quizList",json1);
 		model.addAttribute("answerList", list2);
 		model.addAttribute("subjectList", list3);
-		model.addAttribute("teacherVO", new TeacherVO());
 		
 		return "cbt_teacher/quiz";
+	}
+	
+	// 문제에 대한 지문들 출력
+	@PostMapping("textcontent")
+	@ResponseBody
+	public List<TeacherVO> textContentList(@RequestParam("qId") int qId){
+		return examService.answerList(qId);
 	}
 
 	// 등록된 문제 필터링해서 출력
@@ -71,21 +77,12 @@ public class ExamController {
 		return examService.getQuizFilter(sName);
 	}
 
-	// 문제 단건조회를 문제 세부보기 모달창으로 적용시켜야 한다
+	// 문제 단건조회
 	@PostMapping("quizinfo")
 	@ResponseBody
 	public List<TeacherVO> quizInfo(@RequestParam("qId") int qId) {
 		return examService.getQuizInfo(qId);
 	}
-	
-	//@GetMapping("quizinfo")
-	//@ResponseBody
-	//public TeacherVO quinInfo(TeacherVO teacherVO, Model model) {
-		//TeacherVO findVO = examService.quizInfo(teacherVO);
-		//model.addAttribute("quizinfo",findVO);
-		//return findVO;
-	//}
-	
 
 	// 과목 추가 - 처리
 	@PostMapping("subjectinsert")
