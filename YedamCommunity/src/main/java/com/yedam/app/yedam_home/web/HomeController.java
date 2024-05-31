9e431ca61cd1c0ac54ebd12607e2160888747957
SeoYeongHoon <tjdudgns1996@naver.com>
2024-06-01 00:52:49 +0900

    improve design, layout, added video tags

--- YedamCommunity/src/main/java/com/yedam/app/yedam_home/web/HomeController.java
+++ YedamCommunity/src/main/java/com/yedam/app/yedam_home/web/HomeController.java
@@ -3,14 +3,13 @@ package com.yedam.app.yedam_home.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class HomeController {
 	
-	@Autowired
-	
-	@GetMapping("/home")
+	@GetMapping("/all/home")
 	public String homePage() {
 		
 		return "mainPages/home";
